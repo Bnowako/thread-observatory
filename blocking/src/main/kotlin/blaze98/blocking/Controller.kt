22c22e9cbf25204bb0b275532e7dbe4f6942ec73
bnowako <blazej.nowakowski@allegro.pl>
2022-11-10 13:56:06 +0100

    Controller improvements

--- blocking/src/main/kotlin/blaze98/blocking/Controller.kt
+++ blocking/src/main/kotlin/blaze98/blocking/Controller.kt
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("blocking")
+@RestController
 class Controller {
 
     @GetMapping("/sleep")
     fun sleep(@RequestParam("sleepTimeMillis", required = false) sleepTimeMillis: Long?) {
+        println("sleep with sleepTimeMilli $sleepTimeMillis")
         Thread.sleep(sleepTimeMillis ?: 5000)
     }
 }
