34ed318085e14137c85ba1abc9149b10b8fe779e
bnowako <blazej.nowakowski@allegro.pl>
2022-11-10 13:43:28 +0100

    Allow user to change sleep times

--- blocking/src/main/kotlin/blaze98/blocking/Controller.kt
+++ blocking/src/main/kotlin/blaze98/blocking/Controller.kt
@@ -1,13 +1,14 @@
 package blaze98.blocking
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("blocking")
 class Controller {
 
     @GetMapping("/sleep")
-    fun sleep() {
-        Thread.sleep(5000)
+    fun sleep(@RequestParam("sleepTimeMillis", required = false) sleepTimeMillis: Long?) {
+        Thread.sleep(sleepTimeMillis ?: 5000)
     }
 }
